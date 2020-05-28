23bf0364421033112bf43d161aacacfe192a197d
xu9449 <xu9449@gmail.com>
2020-05-28 02:46:04 -0400

    cicd1..2

--- webapi/src/test/java/com/sean/debug12/service/ImageServiceTest.java
+++ webapi/src/test/java/com/sean/debug12/service/ImageServiceTest.java
@@ -13,8 +13,8 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-//@SpringBootTest(classes= Appbootstrap.class)
-@SpringBootTest(classes= ImageServiceTest.class)
+@SpringBootTest(classes= Appbootstrap.class)
+
 
 public class ImageServiceTest {
 
