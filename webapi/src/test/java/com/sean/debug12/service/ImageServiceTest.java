beb2485a8d05d7675251d36f2dfadb4cac86197c
xu9449 <xu9449@gmail.com>
2020-05-28 02:41:15 -0400

    cicd1..1

--- webapi/src/test/java/com/sean/debug12/service/ImageServiceTest.java
+++ webapi/src/test/java/com/sean/debug12/service/ImageServiceTest.java
@@ -13,8 +13,9 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes= Appbootstrap.class)
-@Profile("unit")
+//@SpringBootTest(classes= Appbootstrap.class)
+@SpringBootTest(classes= ImageServiceTest.class)
+
 public class ImageServiceTest {
 
     @Autowired
