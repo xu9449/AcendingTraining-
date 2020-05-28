799c65b9305ba787b174ca354f3cea8b408fe6d3
xu9449 <xu9449@gmail.com>
2020-05-28 02:15:54 -0400

    cicd1

--- consumer/src/main/java/com/sean/debug12/service/WorkApplication.java
+++ consumer/src/main/java/com/sean/debug12/service/WorkApplication.java
@@ -4,9 +4,9 @@ package com.sean.debug12.service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class WorkApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(WorkApplication.class, args);
-    }
-}
+//@SpringBootApplication
+//public class WorkApplication {
+//    public static void main(String[] args) {
+//        SpringApplication.run(WorkApplication.class, args);
+//    }
+//}
